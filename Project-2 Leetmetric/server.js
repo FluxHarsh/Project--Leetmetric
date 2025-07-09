7d84e24b660fb2f82182fe4be4c606bbbbc64c57
Harsh Jagtap <harshjagtap843@gmail.com>
2025-07-09 22:19:28 +0530

    Update server.js

--- Project-2 Leetmetric/server.js
+++ Project-2 Leetmetric/server.js
@@ -1,31 +1,35 @@
 const express = require("express");
-const fetch = require("node-fetch");  
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+
+app.use(express.static(__dirname));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/leetcode", async (req, res) => {
   try {
     const response = await fetch("https://leetcode.com/graphql", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
     });
-
     const data = await response.json();
     res.json(data);
   } catch (err) {
-    console.error(" Server Error:", err);
+    console.error("Server Error:", err);
     res.status(500).json({ error: "Something went wrong on the server" });
   }
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
